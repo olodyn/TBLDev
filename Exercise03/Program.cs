eeba090b0563d97b7ab0b9a17dc6f6a7434b7a63
olodyn <33954279+olodyn@users.noreply.github.com>
2021-07-31 13:01:57 +0300

    Add files via upload

---
+++ Exercise03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Exercise02;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string isNegative = "";
+            try
+            {
+                Console.WriteLine("Enter a Number to convert to words");
+                string number = Console.ReadLine();
+                number = Convert.ToDouble(number).ToString();
+
+                if (number.Contains("-"))
+                {
+                    isNegative = "Minus ";
+                    number = number.Substring(1, number.Length - 1);
+                }
+                if (number == "0")
+                {
+                    Console.WriteLine("The number in word format is \nZero Only");
+                }
+                else
+                {
+                    Console.WriteLine("The number in word fomat is \n{0}", isNegative + NumtoWords.ConvertToWords(number));
+                    //numToWords.
+                }
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
